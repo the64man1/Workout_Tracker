4f3d5e520bdeecf25620da34511087acbd2c43f0
maxquintanilla <the64man1@yahoo.com>
2021-06-30 20:52:54 -0500

    correct model, routes for workout and range

--- models/Workout.js
+++ models/Workout.js
@@ -9,8 +9,24 @@ const WorkoutSchema = new Schema({
     },
     exercises: [
         {
-            type: Schema.Types.ObjectId,
-            ref: "Exercise"
+            type: {
+                type: String
+            },
+            name: {
+                type: String
+            },
+            duration: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            }
         }
     ]
 });
--- routes/api.js
+++ routes/api.js
@@ -11,4 +11,24 @@ router.get('/api/workouts', (req, res) => {
       })
 })
 
+router.get('/api/workouts/range', (req, res) => {
+    Workout.find().sort({ _id: -1 }).limit(7)
+      .then(dbWorkout => {
+          res.json(dbWorkout)
+      })
+      .catch(err => {
+          res.json(err)
+      })
+})
+
+// router.get('/api/workouts', (req, res) => {
+//     Workout.find({})
+//       .then(dbWorkout => {
+//           res.json(dbWorkout)
+//       })
+//       .catch(err => {
+//           res.json(err)
+//       })
+// })
+
 module.exports = router;
